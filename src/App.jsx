9ce3266dc0fa53518af0d9b977e344ec58e120aa
Harsh-Chopra <harshchopra9406626920@gmail.com>
2021-10-15 12:00:13 +0530

    File name changeg

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,26 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-      <div className="container-main">
-        <div className="row">
+        <div className="container-main">
+          <div className="row">
 
-          <div className="col-lg-3">
-            <Sidebar />
+            <div className="col-lg-3">
+              <Sidebar />
+            </div>
+
+            <div className="col-lg-9 app__main-content">
+              <Switch>
+                <Route path='/' exact>
+                  <Navbar />
+                </Route>
+                <Route path='/roadmap'>
+                  <Roadmap />
+                </Route>
+              </Switch>
+            </div>
+
           </div>
-
-          <div className="col-lg-9 app__main-content">
-          <Switch>
-              <Route path='/' exact>
-                <Navbar />
-              </Route>
-              <Route path='/roadmap'>
-                <Roadmap/>
-              </Route>
-          </Switch>
-
-          </div>
-
         </div>
-      </div>
       </BrowserRouter>
     </div>
   );
