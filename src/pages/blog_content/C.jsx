2fa225b48ff0ed61f3a60150eedf7a9bf62ee10c
Harsh-Chopra <harshchopra9406626920@gmail.com>
2021-10-18 20:59:44 +0530

    Routed cards

---
+++ src/pages/blog_content/C.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default C
---
+++ src/pages/blog_content/Fullstack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fullstack = () => {
+    return (
+        <div>
+            Full stack
+        </div>
+    )
+}
+
+export default Fullstack
