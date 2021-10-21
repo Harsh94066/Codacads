1036b8ee9a7452eb7644600a59a799cb56c5bf7c
Harsh-Chopra <harshchopra9406626920@gmail.com>
2021-10-21 12:27:57 +0530

    Css c

--- src/pages/blog_content/C.jsx
+++ src/pages/blog_content/C.jsx
@@ -7,7 +7,7 @@ const C = () => {
   return (
     <>
       <div className="website_theme">
-        <h1> Learn C </h1>
+        <h1 id="learn-c"> Learn C </h1>
 
         <section className="section">
           <h2>What is C?</h2>
@@ -26,13 +26,13 @@ const C = () => {
           <div>
             <Highlight className="highlight">
               {`
-        #include < stdio.h >
-        
-        int main(){
-        printf("Hello C Language");
-        return 0;   
-        }
-                  `
+          #include < stdio.h >
+          
+          int main(){
+          printf("Hello C Language");
+          return 0;   
+          }
+                    `
               }
             </Highlight>
           </div>
@@ -42,7 +42,7 @@ const C = () => {
             <li><b>int main():</b> The main() function is the entry point of every program in c language.</li>
             <li><b>return 0:</b> The return 0 statement, returns execution status to the OS. </li>
           </ul>
-          <h5>printf() and scanf() in C</h5>
+          <h6>printf() and scanf() in C</h6>
           <div className="h5-after-margin">
 
             <p>The printf() and scanf() functions are used for input and output in C language. Both functions are inbuilt library functions, defined in stdio.h (header file).</p>
@@ -59,164 +59,160 @@ const C = () => {
               </li>
             </ul>
           </div>
-
-        </section>
-
-        <section className="section">
-          <h5>Program to print cube of given number</h5>
+          <h6>Program to print cube of given number</h6>
           <div>
             <Highlight>
               {`
-        #include<stdio.h>
-        int main()
-        {
-            int num;
-            printf("enter a number:");
-            scanf("%d",&num);
-            printf("cube of number is:%d ",num*num*num);
-            return 0;
-        }
-                `}
+          #include<stdio.h>
+          int main()
+          {
+              int num;
+              printf("enter a number:");
+              scanf("%d",&num);
+              printf("cube of number is:%d ",num*num*num);
+              return 0;
+          }
+                  `}
             </Highlight>
           </div>
 
           <div  >
             <Highlight>
               {`
-        Output
-        enter a number: 5
-        cube of number is: 125
-                  `}
-                </Highlight>
-              </div>
-
-            </section>
-
-            <section className="section">
-              <h2 >Variables and Keywords in C</h2>
-              <hr />
-              <ul className='p-bullets'>
-                <li> A variable is a name of the memory location. It is used to store data. Its value can be changed, and it can be reused many times.</li>
-                <li>Keywords are predefined, reserved words in C language and each of which is associated with specific features.</li>
-                <li>For more understanding you can refer <a href="https://www.geeksforgeeks.org/variables-and-keywords-in-c/" target="_blank">this gfg blog.</a> </li>
-              </ul>
-            </section>
-
-            <section className="section">
-              <h2>Data Types</h2>
-              <hr />
-              <p>A data type specifies the type of data that a variable can store such as integer, floating, character, etc.
-              </p>
-              <img src="/images/DatatypesC.jpg" style={{ marginLeft: "4.5rem", marginBottom: "2.5rem" }} height="450" width="550" />
-              <Table striped bordered variant="light" style={{ width: "600px", marginLeft: "5rem" }}>
-                <thead style={{ backgroundColor: "hsl(240deg 78% 63%)" }}>
-                  <tr>
-                    <th>Types</th>
-                    <th>Data Types</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Basic Data Type</td>
-                    <td>int, char, float, double</td>
-                  </tr>
-                  <tr>
-                    <td>Derived Data Type</td>
-                    <td>array, pointer, structure, union</td>
-                  </tr>
-                  <tr>
-                    <td>Enumeration Data Type</td>
-                    <td>enum</td>
-                  </tr>
-                  <tr>
-                    <td>Void Data Type</td>
-                    <td>void</td>
-                  </tr>
-                </tbody>
-              </Table>
-              <p>For more understanding you can refer <a href="https://www.geeksforgeeks.org/data-types-in-c/" target="_blank">this gfg blog.</a></p>
-            </section>
-
-            <section className="section">
-              <h2> Identifiers </h2>
-              <hr />
-              <p>C identifiers represent the name in the C program, for example, variables, functions, arrays, structures, unions, labels, etc.
-                <br />
-                For more understanding you can refer <a href="https://www.javatpoint.com/c-identifiers" target="_blank">this blog.</a>
-              </p>
-            </section>
-
-            <section className="section">
-              <h2>Operators</h2>
-              <hr />
-              <p>Operators are the foundation of any programming language. Thus the functionality of C/C++ programming language is incomplete without the use of operators. <br /> For more understanding you can refer <a href="https://www.geeksforgeeks.org/operators-c-c/" target="_blank">this gfg blog.</a>
-              </p>
-            </section>
-
-            <section className="section">
-              <h2>Comments</h2>
-              <hr />
-              <p>Comments in C language are used to provide information about lines of code. It is widely used for documenting code.
-                <br /> For more understanding you can refer <a href="https://www.javatpoint.com/comments-in-c" target="_blank">this blog.</a>
-              </p>
-            </section>
-
-            <section className="section">
-              <h2>C Format Specifier</h2>
-              <hr />
-              <p>The Format specifier is a string used in the formatted input and output functions. <br />For more understanding you can refer <a href="https://www.javatpoint.com/c-format-specifier" target="_blank"> this blog. </a> </p>
-              <h5>Read and try the various sections as following:</h5>
-              <ul>
-                <li ><a href="https://www.javatpoint.com/constants-in-c" target="_blank"> C constants </a></li>
-                <li ><a href="https://www.javatpoint.com/literals-in-c" target="_blank"> C literals </a></li>
-                <li ><a href="https://www.javatpoint.com/static-in-c" target="_blank"> C static </a></li>
-                <li ><a href="https://www.javatpoint.com/programming-errors-in-c" target="_blank"> C Programming errors </a> </li>
-              </ul>
-            </section>
-
-            <section className="section">
-              <h2> C Control Statement</h2>
-              <hr />
-              <p>There are four types of control statements:</p>
-              <ul >
-                <li >Sequential control statements</li>
-                <li >Conditional/Decision control statements</li>
-                <li >Switch control statements</li>
-                <li >Looping control statements</li>
-              </ul>
-              <p>For more understanding you can refer this <a href="https://www.javatpoint.com/c-if-else" target="_blank">C control statements</a></p>
-            </section>
-
-            <section className="section">
-              <h2>C Functions</h2>
-              <hr />
-              <ul className='p-bullets'>
-                <li>Function is a self contained block of statements.</li>
-                <li>Every function performs some specific task.</li>
-                <li>Function can transform data.</li>
-                <li>Function is also known as module or routines or methods.</li>
-                <li>We can call function 'n' number of times.</li>
-              </ul>
-              <p>For more understanding you can refer this <a href="https://www.javatpoint.com/functions-in-c" target="_blank">C Functions</a>
-              </p>
-            </section>
-
-            <section className="section">
-              <h2>Advanced concepts in C</h2>
-              <hr />
-              <ul className='p-bullets'>
-                <li><a href="https://www.javatpoint.com/c-array" target="_blank"> C Arrays </a></li>
-                <li><a href="https://www.javatpoint.com/c-pointers" target="_blank"> C Pointers </a></li>
-                <li><a href="https://www.javatpoint.com/c-strings" target="_blank"> C String </a></li>
-                <li><a href="https://www.javatpoint.com/dynamic-memory-allocation-in-c" target="_blank"> C Dynamic Memory </a></li>
-                <li><a href="https://www.javatpoint.com/c-math" target="_blank"> C Math functions </a></li>
-                <li><a href="https://www.javatpoint.com/structure-in-c" target="_blank"> C Structure & Union </a></li>
-                <li><a href="https://www.javatpoint.com/c-preprocessor" target="_blank"> C Preprocessor </a></li>
-              </ul>
-              <p>Otherwise, you can learn C, by watching <a href="https://www.youtube.com/playlist?list=PLe_7x5eaUqtWp9fvsxhC4XIkoR3n5A-sF" target="_blank">Saurabh Shukla C videos</a> </p>
-            </section>
-
+          Output
+          enter a number: 5
+          cube of number is: 125
+                    `}
+            </Highlight>
           </div>
+
+        </section>
+
+        <section className="section">
+          <h2 >Variables and Keywords in C</h2>
+          <hr />
+          <ul className='p-bullets'>
+            <li> A variable is a name of the memory location. It is used to store data. Its value can be changed, and it can be reused many times.</li>
+            <li>Keywords are predefined, reserved words in C language and each of which is associated with specific features.</li>
+            <li>For more understanding you can refer <a href="https://www.geeksforgeeks.org/variables-and-keywords-in-c/" target="_blank">this gfg blog.</a> </li>
+          </ul>
+        </section>
+
+        <section className="section">
+          <h2>Data Types</h2>
+          <hr />
+          <p>A data type specifies the type of data that a variable can store such as integer, floating, character, etc.
+          </p>
+          <img src="/images/DatatypesC.jpg" />
+          <Table striped bordered variant="light" className="table">
+            <thead >
+              <tr>
+                <th>Types</th>
+                <th>Data Types</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Basic Data Type</td>
+                <td>int, char, float, double</td>
+              </tr>
+              <tr>
+                <td>Derived Data Type</td>
+                <td>array, pointer, structure, union</td>
+              </tr>
+              <tr>
+                <td>Enumeration Data Type</td>
+                <td>enum</td>
+              </tr>
+              <tr>
+                <td>Void Data Type</td>
+                <td>void</td>
+              </tr>
+            </tbody>
+          </Table>
+          <p>For more understanding you can refer <a href="https://www.geeksforgeeks.org/data-types-in-c/" target="_blank">this gfg blog.</a></p>
+        </section>
+
+        <section className="section">
+          <h2> Identifiers </h2>
+          <hr />
+          <p>C identifiers represent the name in the C program, for example, variables, functions, arrays, structures, unions, labels, etc.
+            <br />
+            For more understanding you can refer <a href="https://www.javatpoint.com/c-identifiers" target="_blank">this blog.</a>
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>Operators</h2>
+          <hr />
+          <p>Operators are the foundation of any programming language. Thus the functionality of C/C++ programming language is incomplete without the use of operators. <br /> For more understanding you can refer <a href="https://www.geeksforgeeks.org/operators-c-c/" target="_blank">this gfg blog.</a>
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>Comments</h2>
+          <hr />
+          <p>Comments in C language are used to provide information about lines of code. It is widely used for documenting code.
+            <br /> For more understanding you can refer <a href="https://www.javatpoint.com/comments-in-c" target="_blank">this blog.</a>
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>C Format Specifier</h2>
+          <hr />
+          <p>The Format specifier is a string used in the formatted input and output functions. <br />For more understanding you can refer <a href="https://www.javatpoint.com/c-format-specifier" target="_blank"> this blog. </a> </p>
+          <h5>Read and try the various sections as following:</h5>
+          <ul>
+            <li ><a href="https://www.javatpoint.com/constants-in-c" target="_blank"> C constants </a></li>
+            <li ><a href="https://www.javatpoint.com/literals-in-c" target="_blank"> C literals </a></li>
+            <li ><a href="https://www.javatpoint.com/static-in-c" target="_blank"> C static </a></li>
+            <li ><a href="https://www.javatpoint.com/programming-errors-in-c" target="_blank"> C Programming errors </a> </li>
+          </ul>
+        </section>
+
+        <section className="section">
+          <h2> C Control Statement</h2>
+          <hr />
+          <p>There are four types of control statements:</p>
+          <ul >
+            <li >Sequential control statements</li>
+            <li >Conditional/Decision control statements</li>
+            <li >Switch control statements</li>
+            <li >Looping control statements</li>
+          </ul>
+          <p>For more understanding you can refer this <a href="https://www.javatpoint.com/c-if-else" target="_blank">C control statements</a></p>
+        </section>
+
+        <section className="section">
+          <h2>C Functions</h2>
+          <hr />
+          <ul className='p-bullets'>
+            <li>Function is a self contained block of statements.</li>
+            <li>Every function performs some specific task.</li>
+            <li>Function can transform data.</li>
+            <li>Function is also known as module or routines or methods.</li>
+            <li>We can call function 'n' number of times.</li>
+          </ul>
+          <p>For more understanding you can refer this <a href="https://www.javatpoint.com/functions-in-c" target="_blank">C Functions</a>
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>Advanced concepts in C</h2>
+          <hr />
+          <ul className='p-bullets'>
+            <li><a href="https://www.javatpoint.com/c-array" target="_blank"> C Arrays </a></li>
+            <li><a href="https://www.javatpoint.com/c-pointers" target="_blank"> C Pointers </a></li>
+            <li><a href="https://www.javatpoint.com/c-strings" target="_blank"> C String </a></li>
+            <li><a href="https://www.javatpoint.com/dynamic-memory-allocation-in-c" target="_blank"> C Dynamic Memory </a></li>
+            <li><a href="https://www.javatpoint.com/c-math" target="_blank"> C Math functions </a></li>
+            <li><a href="https://www.javatpoint.com/structure-in-c" target="_blank"> C Structure & Union </a></li>
+            <li><a href="https://www.javatpoint.com/c-preprocessor" target="_blank"> C Preprocessor </a></li>
+          </ul>
+          <p>Otherwise, you can learn C, by watching <a href="https://www.youtube.com/playlist?list=PLe_7x5eaUqtWp9fvsxhC4XIkoR3n5A-sF" target="_blank">Saurabh Shukla C videos</a> </p>
+        </section>
+
+      </div>
     </>
   )
 }
