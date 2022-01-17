9b2c5060d96c5e0b014e24a5716a8e8c5fadb2b9
Harsh-Chopra <harshchopra9406626920@gmail.com>
2022-01-17 23:23:33 +0530

    Images url changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,17 +36,17 @@ const Navbar = () => {
 
                 <div className="front-page-cards">
                     <div className="single-card">
-                        <img src="https://media.istockphoto.com/vectors/software-web-development-programming-concept-abstract-programming-vector-id1275744786" alt="error" height="200" width="250" />
+                        <img src="/images/Algorithm.jpg" alt="error" height="200" width="250" />
                         <h4>Understanding algorithms</h4>
                         <p>Understand all the algorithms and data structures well. You should be able to figure out when to use a particular data structure or algorithm. It's rightly said that "You don't have to be great to start, but you have to start to be great."</p>
                     </div>
                     <div className="single-card">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyZVBx3QcXf1svHBvKZNedP05xFevWXsrt4nPZCaiO6AaEXIn5pM8Uoa1cGANj9DFMYzc&usqp=CAU" alt="error" height="200" width="270" />
+                        <img src="/images/Practice.jpg" alt="error" height="200" width="270" />
                         <h4>Practice</h4>
                         <p>Practice the set of questions given to you, after each session. Make sure, you try them, by yourself. You should look at the editorial, hints, etc. only when you have given enough time to a question and still, you are not able to figure out the correct approach. Never say "I can't" because you can and you will, it takes practice to become perfect.</p>
                     </div>
                     <div className="single-card">
-                        <img src="https://media.istockphoto.com/vectors/student-sitting-at-table-vector-id665039932?k=20&m=665039932&s=612x612&w=0&h=g-LaeCLxQuf5ROEtHFWYZgr5pTu4rEWyJMcMC6O12Tk=" alt="error" height="200" width="200" />
+                        <img src="/images/Contest.jpg" alt="error" height="200" width="200" />
                         <h4>Regular contests</h4>
                         <p>As, giving timed contests, helps to handle problems under pressure and boosts your time management skills. Give the contests on various online judges, regularly, as instructed here. Try to upsolve 1 or 2 problems after every contest.</p>
                     </div>
